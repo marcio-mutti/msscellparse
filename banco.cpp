77803b17d21a7ed8c86682a4d8cb080d44cd8ce4
Mracio Mutti <marcio.mutti@gmail.com>
2016-11-14 21:29:43 +0000

    Advances toward PostgreSQL Interface.

--- banco.cpp
+++ banco.cpp
@@ -1,22 +1,124 @@
 #include "banco.hpp"
+#include <fstream>
+#include <iostream>
 
 using namespace std;
 
 void pgsql::handle_command(PGresult* result, const std::string& context="") throw(const runtime_error&) {
-	bool has_error(false);
-	string error_message;
-	if (PQresultStatus(result) != PGRES_COMMAND_OK) {
-		error_message=PQresultErrorMessage(result);
-		if (context.size() > 0) {
-			error_message+='\n';
-			error_message+=context;
-		}
-		has_error=true;
-	}
-	PQclear(result);
-	if (has_error) throw (runtime_error(error_message));
+    bool has_error(false);
+    string error_message;
+    if (PQresultStatus(result) != PGRES_COMMAND_OK) {
+        error_message=PQresultErrorMessage(result);
+        if (context.size() > 0) {
+            error_message+='\n';
+            error_message+=context;
+        }
+        has_error=true;
+    }
+    PQclear(result);
+    if (has_error) throw (runtime_error(error_message));
 }
 
-pgsql::query_result::query_result() : n_columns(0), n_rows(0)
-{
+pgsql::query_result::query_result() : success_(false), n_columns(0), n_rows(0) {}
+void pgsql::query_result::clear() {
+    success_ = false;
+    n_columns = n_rows = 0;
+    column_names.clear();
+    fetched_values.clear();
 }
+const bool pgsql::query_result::success() const         { return success_; }
+void pgsql::query_result::load_from_result(PGresult* result) throw(const runtime_error&) {
+    bool query_error(false);
+    string error_message;
+    clear();
+    if (PQresultStatus(result) !=  PGRES_TUPLES_OK) {
+        query_error = true;
+        error_message = PQresultErrorMessage(result);
+        success_ = false;
+    } else {
+        n_rows = PQntuples(result);
+        n_columns = PQnfields(result);
+        for(size_t i = 0;i != n_columns; ++i) {
+            column_names.push_back(PQfname(result, i));
+        }
+        for (size_t j = 0;j != n_rows;++j) {
+            vector<string> this_result;
+            this_result.resize(n_columns);
+            for (size_t i = 0;i != n_columns;++i) {
+                this_result.at(i) = PQgetvalue(result, j, i);
+            }
+            fetched_values.push_back(this_result);
+        }
+    }
+    PQclear(result);
+    if (query_error) throw(runtime_error(error_message));
+}
+
+pgsql::db_connection::db_connection() : dbconn(nullptr) {}
+pgsql::query_result::~query_result()                    {}
+void pgsql::db_connection::load_database_config(const std::string& filename) {
+  ifstream configfile(filename);
+  bool titulo(true);
+  if (configfile.fail()) {
+    cerr << "Não foi possível abrir o arquivo " << filename << "." <<  endl;
+    return;
+  }
+  while (configfile.good()) {
+    string keyword, parameter;
+    if (titulo)
+      continue;
+    configfile >> keyword >> parameter;
+    connection_parameters.insert({keyword, parameter});
+  }
+  configfile.close();
+}
+void pgsql::db_connection::connect_to_database() throw (const runtime_error&) {
+    const char ** keywords(nullptr);
+    const char ** parameters(nullptr);
+    int n(0);
+    if (connection_parameters.size() ==  0)
+        throw(runtime_error("Não foram fornecidos os dados de conexão."));
+    if (dbconn !=  nullptr)
+        PQfinish(dbconn);
+    keywords = new const char*[connection_parameters.size()+1];
+    parameters = new const char*[connection_parameters.size()+1];
+    for (map<string, string>::iterator citer = connection_parameters.begin();citer != connection_parameters.end();++citer) {
+        keywords[n] = citer->first.c_str();
+        parameters[n++] = citer->second.c_str();
+    }
+    keywords[n] = 0;
+    parameters[n] = 0;
+    dbconn = PQconnectdbParams(keywords, parameters, 0);
+    if (PQstatus(dbconn) != CONNECTION_OK) {
+        throw(runtime_error(PQerrorMessage(dbconn)));
+    }
+    PQfinish(dbconn);
+    dbconn = nullptr;
+}
+void pgsql::db_connection::execute_command(const std::string& query) throw(const runtime_error&)
+    { handle_command(PQexec(dbconn, query.c_str()), query); }
+pgsql::query_result pgsql::db_connection::execute_returning_query(const std::string& query) throw (const runtime_error&) {
+    pgsql::query_result result;
+    result.load_from_result(PQexec(dbconn, query.c_str()));
+    return result;
+}
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
--- banco.hpp
+++ banco.hpp
@@ -1,21 +1,43 @@
 #ifndef BANCO_H
 #define BANCO_H
 
+# include <map>
 #include <stdexcept>
 #include <string>
+# include <vector>
 
 #include <libpq-fe.h>
 
 namespace pgsql {
-	class query_result {
-	public:
-		query_result();
-		~query_result();
-	protected:
-		size_t n_columns, n_rows;
-	};
-	void handle_command(PGresult* result, const std::string& context) throw(const std::runtime_error&);
-	
+class query_result {
+public:
+    query_result();
+    ~query_result();
+    void clear();
+    const bool success() const;
+    void load_from_result(PGresult *) throw (const std::runtime_error&);
+protected:
+    bool success_;
+    size_t n_columns, n_rows;
+    std::vector<std::string> column_names;
+    std::vector<std::vector<std::string>> fetched_values;
+};
+void handle_command(PGresult* result, const std::string& context) throw(const std::runtime_error&);
+class db_connection {
+public:
+    db_connection();
+    ~db_connection();
+    void load_database_config(const std::string&);
+    void connect_to_database() throw (const std::runtime_error&);
+    void execute_command(const std::string&) throw(const std::runtime_error&);
+    query_result execute_returning_query(const std::string&) throw(const std::runtime_error&);
+    void prepare_statement(const std::string&,  const std::string&) throw (const std::runtime_error&);
+    void execute_prepared_statement(const std::string&,  const std::vector<std::string>&) throw (const std::runtime_error&);
+    query_result execute_returning_prepared_statement(const std::string&,  const std::vector<std::string>&) throw (const std::runtime_error&);
+protected:
+    PGconn * dbconn;
+    std::map<std::string, std::string> connection_parameters;
+};
 };
 
 #endif
